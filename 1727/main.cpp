4ee0fde526e03a97976d8a445474914cdb2480cf
Shuo Yan <yanshuo1987@gmail.com>
2014-09-07 22:49:54 -0400

    POJ#1727 Part 1, wrong answer

---
+++ 1727/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct Point{
+    int _x;
+    int _y;
+    Point(int x, int y):_x(x), _y(y){}
+    bool operator<(const Point& right) const {
+        return _x<right._x || (_x==right._x && _y<right._y);
+    }
+};
+
+vector<Point> data;
+
+bool checkCurY(int y, int sources) {
+    int count = 0;
+    int cur_left, cur_right, new_left, new_right;
+    for (vector<Point>::iterator it=data.begin(); it!=data.end(); ++it) {       
+        new_left = it->_x + (y-it->_y);
+        new_right = it->_x + (it->_y-y);
+        if (it==data.begin() || cur_right<new_left) {
+            cur_left = new_left;
+            cur_right = new_right;
+            count++;
+        } else if (cur_left<=new_left && cur_right<=new_right) {
+            cur_left = new_left;
+        } else if (cur_left<=new_left && cur_right>=new_right) {
+            cur_left = new_left;
+            cur_right = new_right;
+        } 
+        if (count > sources) return false;
+    }
+    return true;
+}
+
+int main (){
+    int cases;
+    cin >> cases;
+    for (int i=1; i<=cases; i++) {
+        data.clear();
+        int x, y, minY = -1000000, maxY = 1000000, result, events, sources;
+        cin >> events >> sources;
+        while(events--) {
+            cin >> y >> x;
+            maxY = y < maxY ? y : maxY;
+            data.push_back(Point(x, y));
+        }
+        //Binary search the result
+        while(minY <= maxY) {
+            int middle = (minY+maxY)/2;
+            if (checkCurY(middle, sources)) {
+                result = middle;
+                minY = middle + 1;
+            } else {
+                maxY = middle - 1;
+            }
+        }
+        cout << "Case " << i << ": "<< result << endl;
+    }
+}
